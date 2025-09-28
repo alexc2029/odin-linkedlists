fb51e8c685f46765e7a6ce98f3b82c8aa9a9c324
alexc2029 <alex2akec@gmail.com>
2025-09-28 12:40:49 +0300

    Add list append method

--- linked-list.js
+++ linked-list.js
@@ -1,1 +1,18 @@
-import { Node } from "./node";
+import { Node } from "./node.js";
+
+class LinkedList {
+	constructor() {
+		this.listArr = [];
+		this.head = null;
+	}
+	append(value) {
+		let node = new Node(value);
+		let i = 0;
+		while (i < this.listArr.length && this.listArr[i].nextNode != null) i++;
+		this.listArr[i] = node;
+	}
+}
+
+let list1 = new LinkedList();
+list1.append(1);
+console.log(list1.listArr);
